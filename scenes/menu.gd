143980056028d2fb09d9f9eca974b35d5db41f12
Amaury <hauler.amaury@gmail.com>
2026-07-19 01:06:45 +0200

    changed script menu

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file('res://scenes/test_scene.tscn')
+	get_tree().change_scene_to_file('res://scenes/test_with_asset_scene.tscn')
 
 
 func on_credits_pressed() -> void:
