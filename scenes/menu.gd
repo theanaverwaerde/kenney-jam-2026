75d9bc2aa18d50251b6bea8c1430238f96eb787f
Amaury <hauler.amaury@gmail.com>
2026-07-18 22:56:04 +0200

    add resources on menu buttons and interactions

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -16,8 +16,12 @@ func _on_play_pressed() -> void:
 
 
 func on_credits_pressed() -> void:
-	print('Credits pressed')
+	$"Credits Scene".visible = true
 
 
 func _on_controls_pressed() -> void:
-	print('Controls pressed')
+	$"Controls Scene".visible = true
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file('res://scenes/menu.tscn')
